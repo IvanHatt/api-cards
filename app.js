ebb9ad0b5d2079d215d5a4349dbaa25ac163a84b
Ivan Hattemer <ivanhattemer@gmail.com>
2021-01-10 14:37:31 +0200

    added cors as dev dep

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const userRouter = require("./routes/userRouter");
 const authRouter = require("./routes/authRouter");
 const profRouter = require("./routes/profRouter");
 const app = express();
-const PORT = 3000;
+const PORT = 3010;
 
 mongoose
   .connect("mongodb://localhost/final_project", {
@@ -16,6 +17,7 @@ mongoose
   .then(() => console.log("Succesfully connected to MongoDB"))
   .catch((err) => console.log("Could not connect to MongoDB"));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRouter);
